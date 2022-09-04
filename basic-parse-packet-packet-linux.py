f57404394ebb0cabfa05364224942a21beda24a0
gilbertokoerbes <gilbertokoerbes@gmail.com>
2022-09-04 18:03:44 -0300

    add ethernet_head

--- basic-parse-packet-packet-linux.py
+++ basic-parse-packet-packet-linux.py
@@ -1,11 +1,12 @@
-from struct import *
+import struct
 import sys
 import socket
+import getmac
 
 def ethernet_head(raw_data):
  dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
- dest_mac = get_mac_addr(dest)
- src_mac = get_mac_addr(src)
+ dest_mac = getmac.get_mac_addr(dest)
+ src_mac = getmac.get_mac_addr(src)
  proto = socket.htons(prototype)
  data = raw_data[14:]
  return dest_mac, src_mac, proto, data 
