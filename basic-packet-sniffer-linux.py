e082acc8ca2ca05327cf718ec58236cdcaabb7da
gilbertokoerbes <gilbertokoerbes@gmail.com>
2022-09-04 18:01:14 -0300

    add ethernet_head

--- basic-packet-sniffer-linux.py
+++ basic-packet-sniffer-linux.py
@@ -1,4 +1,11 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-while True:
- print(s.recvfrom(65565))
+
+def main():
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+     raw_data, addr = s.recvfrom(65535)
+     eth = ethernet(raw_data)
+     print('\nEthernet Frame:')
+     print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1],
+    eth[2]))
+main()
---
+++ basic-parse-packet-packet-linux.py
@@ -0,0 +1,21 @@
+from struct import *
+import sys
+import socket
+
+def ethernet_head(raw_data):
+ dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+ dest_mac = get_mac_addr(dest)
+ src_mac = get_mac_addr(src)
+ proto = socket.htons(prototype)
+ data = raw_data[14:]
+ return dest_mac, src_mac, proto, data 
+
+def main():
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+     raw_data, addr = s.recvfrom(65535)
+     eth = ethernet_head(raw_data)
+     print('\nEthernet Frame:')
+     print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1],
+    eth[2]))
+main()
