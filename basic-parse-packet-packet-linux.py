417a1f5d925234886ca0246cce5883f0f3349bff
gilbertokoerbes <gilbertokoerbes@gmail.com>
2022-09-05 18:40:02 -0300

    Corrigido pacote ethernet

--- basic-parse-packet-packet-linux.py
+++ basic-parse-packet-packet-linux.py
@@ -1,15 +1,18 @@
 import struct
 import sys
 import socket
-import getmac
+import codecs
 
 def ethernet_head(raw_data):
  dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
- dest_mac = getmac.get_mac_addr(dest)
- src_mac = getmac.get_mac_addr(src)
- proto = socket.htons(prototype)
+ print ("Dest",codecs.encode(dest, 'hex'))
+ print ("Src",codecs.encode(src, 'hex'))
+ #dest_mac = get_mac_addr(dest)
+ #src_mac = get_mac_addr(src)
+ #proto = socket.htons(prototype)
+ proto = prototype
  data = raw_data[14:]
- return dest_mac, src_mac, proto, data 
+ return dest, src, proto, data 
 
 def main():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
